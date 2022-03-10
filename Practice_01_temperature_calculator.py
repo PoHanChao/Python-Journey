f6857a842fade00d6ce96d8d44834ca7953c41a6
Po Han Chao <r03449013@gmail.com>
2022-03-10 21:31:39 +0800

    Practice_01

---
+++ Practice_01_temperature_calculator.py
@@ -0,0 +1,7 @@
+#Temperature Change from Celsius to Fahrenheit
+#Imput Celsius temperature(C) and get Fahrenheit
+c = input('Please input Celsius Temperature: ') 
+c = float(c)
+f = c * 9/5  + 32 
+print('Fahrenheit temperature is:', f)
+
