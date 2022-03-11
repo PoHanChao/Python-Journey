5b4ac33e0cf34abb146d71e71c644a05a37c033f
Po Han Chao <r03449013@gmail.com>
2022-03-11 23:06:44 +0800

    Final version

--- Practice_password_three_times.py
+++ Practice_password_three_times.py
@@ -1,12 +1,17 @@
 password = 'a123456'
-i = 3 #
+i = 3 # 剩餘機會
 while i > 0:
+	i -= 1  #改放在這裡，美問一次減少一次
 	pwd = input('請輸入密碼: ')
 	if pwd == password:
 		print('登入成功!')
 		break #逃出迴圈
 	else:
-		i -= 1  #i要放在print之前，這樣才會有"剩下i-1"次
-		print('密碼錯誤，還有', i, '次機會')
+		print('密碼錯誤!')
+		if i > 0:
+			print('還有', i, '次機會')
+		else:
+			print('帳號封鎖')
+
 		
 		
