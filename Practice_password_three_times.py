dffec68f349ae6faffccba1daab3e7d997662e61
Po Han Chao <r03449013@gmail.com>
2022-03-11 23:01:05 +0800

    while i > 0

--- Practice_password_three_times.py
+++ Practice_password_three_times.py
@@ -1,6 +1,6 @@
 password = 'a123456'
 i = 3 #
-while True:
+while i > 0:
 	pwd = input('請輸入密碼: ')
 	if pwd == password:
 		print('登入成功!')
@@ -8,7 +8,5 @@ while True:
 	else:
 		i -= 1  #i要放在print之前，這樣才會有"剩下i-1"次
 		print('密碼錯誤，還有', i, '次機會')
-		if i == 0:
-			break
-
+		
 		
