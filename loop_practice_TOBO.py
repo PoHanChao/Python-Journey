609e825a79f12ebe80890a098d772a09b9202f50
Po Han Chao <r03449013@gmail.com>
2022-03-11 19:40:15 +0800

    For girlfriend

---
+++ loop_practice_TOBO.py
@@ -0,0 +1,106 @@
+print('歡迎搭乘土撥鼠航空 ')
+print()
+name = input ('請問您叫甚麼名字? ')
+print()
+print('你好啊', name, '，歡迎搭乘土撥鼠航空') 
+
+ 
+
+while True:
+
+    penut = input ('請問你要吃花生米嗎? ')
+
+    if penut == 'Yes' or  penut == '要':
+        print()
+        print('逼晡，我們沒有附花生米噢，下次記得去買')
+        break
+    elif penut == 'No' or  penut == '不要':
+        print()
+        print('逼晡，剛好我們沒有附花生米')
+        break
+    else:
+        print()
+        print('比餔?')
+
+print()
+print('各位乘客請注意')
+print('現在土撥鼠機長要進行嚴肅又成熟的學術討論') 
+
+while True:
+    corgi = input ('請問貓跟狗哪一個比較可愛? ')
+    if corgi == '貓':
+        print()
+        print()
+        print('逼晡，正確答案')
+
+        break
+
+    elif corgi == '狗':
+        print()
+        print()
+        print('逼晡，正確答案是貓')
+        break
+
+    elif corgi == '土撥鼠':
+        print()
+        print()
+        print('逼晡，給你一個翰寶幣!')
+        break
+
+    else:
+        print()
+        print()
+        print('比餔?')
+
+
+print()
+print()
+print('各位乘客，土撥鼠航空即將抵達寶貝蛋窩，感謝您的搭乘，在您離開以前，您可以做以下事情')
+print('1. 直接離開')
+print('2. 摸摸小翰寶的頭')
+print('3. 說寶寶話')
+print('4. 抱抱')
+
+       
+
+while True:
+
+    choice = input ('你打算怎麼做呢?:')
+
+    if choice =='1' :
+        print()
+        print()
+        print('必餔!必餔!必餔!')
+        print()
+        print('你引發了土撥鼠動亂')
+
+    elif  choice =='2' :
+        print()
+        print()
+        print('小翰寶開心地: 逼餔')
+        print('小翰寶似乎捨不得離開')
+
+ 
+
+    elif  choice =='3' :
+        print()
+        print('小翰寶開心地: 逼餔')
+        print()
+        print('小翰寶: 好喜歡好喜歡')
+
+        break
+
+    elif  choice =='4' :
+        print()
+        print('小翰寶也把你抱緊緊')
+        print()
+        print('小翰寶似乎捨不得離開')
+
+
+    else:
+        print()
+        print('比餔?')
+
+print()
+print()
+print('土撥鼠航空感謝您的搭乘，晚安安')
